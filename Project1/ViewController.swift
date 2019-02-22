6187bdf9c557b69501e86cf81efb5ff445e63dfe
Guga Gongadze <ggong13@freeuni.edu.ge>
2019-02-22 13:08:34 +0400

    Chore: merge

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
     }
 
 

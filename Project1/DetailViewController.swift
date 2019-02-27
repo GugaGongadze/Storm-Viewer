85fcab40e8e9a32eee388fca447fdfc8678da312
GugaGongadze <ggong13@freeuni.edu.ge>
2019-02-27 11:41:09 +0400

    Feat: large title and resizing images

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -14,10 +14,24 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
     }
     
 

69a1b1af930418dd9a13d2426e5de7a79c513eef
GugaGongadze <ggong13@freeuni.edu.ge>
2019-03-10 15:37:20 +0400

    Feat: Add image sharing mechanism

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -18,6 +18,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         title = "Picture \(imagePosition! + 1) of \(imageCount!)"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         navigationItem.largeTitleDisplayMode = .never
 
         if let imageToLoad = selectedImage {
@@ -36,15 +37,14 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
-    */
-
 }
